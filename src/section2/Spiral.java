79c3e2167cc88dcf015108e3579b8bd5b4332f2c
root <root@e2bfd56091b2>
2020-05-06 02:58:19 +0000

    I need to finish basically all of Spiral

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -12,15 +12,17 @@ public class Spiral {
 		// 3. Set the robot to go at max speed (100)
 		chad.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-	
-			// 7. Change the pen color to random
-			//Hermela
 		
+		
+		
+			// 7. Change the pen color to random
+			chad.setRandomPenColor();
+			
 			// 6. Move the robot 5 times the loop counter (5*i)
 			
 			// 2. Turn the robot 360/7 degrees to the right
 		
 			// 8. Set the pen width to i
-			
+		
 	}
 }

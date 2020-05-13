f38a4a14b53f8f75e8c3ee651ad0d5f91f5807b1
root <root@22dc1730396a>
2020-05-13 03:00:22 +0000

    quiz

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -9,15 +9,15 @@ public class MadlibsOfTheAmazon {
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
 		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha-infested river, here's how to do it...");
 		// Get the user to enter an adjective
-		JOptionPane.showInputDialog(null, "Enter and adjective:");
+		String adjective= JOptionPane.showInputDialog(null, "Enter and adjective:");
 		// Get the user to enter a type of liquid
-		JOptionPane.showInputDialog(null, "Enter and liquid:");
+		String liquid=JOptionPane.showInputDialog(null, "Enter and liquid:");
 		// Get the user to enter a body part
-		JOptionPane.showInputDialog(null, "Enter and bodyPart:");
+		String bodyPart=JOptionPane.showInputDialog(null, "Enter and bodyPart:");
 		// Get the user to enter a verb
-		JOptionPane.showInputDialog(null, "Enter and verb:");
+		String verb=JOptionPane.showInputDialog(null, "Enter and verb:");
 		// Get the user to enter a place
-		JOptionPane.showInputDialog(null, "Enter and place:");
+		String place=JOptionPane.showInputDialog(null, "Enter and place:");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
@@ -25,6 +25,13 @@ public class MadlibsOfTheAmazon {
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!
 		
+		// "String is my name " + name +  "    a  " + "     b    " + 9  --> "String is my name Charlie     a      b     9"
+		
+		JOptionPane.showMessageDialog(null, "Piranhas are more " +adjective+ " during the day, " + "\n"
+				+ "so cross the river at night. Piranhas are attracted to fresh " +liquid+ "\n"
+			+ " and will most likely take a bite out of your " +bodyPart+ " if you " +verb+ "\n"
+			+ ". Whatever you do, if you have an open wound, try to find another way to get back \n"
+			+ "to the " +place+ ". Goodluck!");
 		// Make a pop-up for the final story. You can use \n to go to the next line
 		
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,15 +1,26 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
+		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score=0;
 		// 2.  Ask the user a question 
-		
-		// 3.  Use an if statement to check if their answer is correct
-		
+		String input= JOptionPane.showInputDialog(null, "What is 2a-4=-6a?:");
+		// 3.  Use String adjective an if statement to check if their answer is correct
+		if (input.equals(" ")){ 
+			
+
+			
+			
+			
+		}else {
+			
+		}
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
